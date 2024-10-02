5b226fafe005f218d18b90386b63539899c26019
Rajat09Sharma <rajat9.lyfe@gmail.com>
2024-10-03 01:47:46 +0530

    first commit

---
+++ src/components - Copy/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <section id="footer">
+        <div className="footer-conatiner">
+
+        </div>
+    </section>
+  )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../footer.css"
+export default function Footer() {
+  return (
+    <section id="footer">
+        <div className="footer-conatiner">
+
+        </div>
+    </section>
+  )
+}
